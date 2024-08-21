7b138e25b585336b441dea51a8cc94aa0c819c55
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-08-21 21:24:48 +0545

    update all files with new features and fixes

---
+++ index.js
@@ -0,0 +1,21 @@
+//while loop
+// let i = 0;
+// while(i<=10){
+//     console.log(i);
+//     i++
+// }
+
+//for loop
+// let i = 0;
+// for(i=0; i<=10;i++){
+//     console.log(i);
+// }
+
+//for each
+let i = 2;
+for(i;i<=20;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
