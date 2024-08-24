988bc13b9b95c23678df151ed77de0853e41cbb9
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-08-24 07:57:56 +0545

    third commit

--- index.js
+++ index.js
@@ -12,10 +12,15 @@
 // }
 
 //for each
-let i = 2;
-for(i;i<=20;i++){
-    if(i%2==0){
-        console.log(i);
-    }
+// let i = 2;
+// for(i;i<=20;i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }
+// }
+let i = 0;
+let donkey = ["amit", "shah", "niles"];
+for(i;i<=donkey.length;i++){
+    console.log(donkey[i]);
 }
 
