478b163696062f70de189a3ada305db1f63985d6
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2024-08-29 22:25:14 +0545

    second commit

--- index.js
+++ index.js
@@ -1,42 +1,51 @@
-//while loop
+// // while loop
 // let i = 0;
 // while(i<=10){
 //     console.log(i);
 //     i++
 // }
 
-//for loop
+// for loop
 // let i = 0;
 // for(i=0; i<=10;i++){
 //     console.log(i);
 // }
 
-//for each
+// for each
 // let i = 2;
 // for(i;i<=20;i++){
 //     if(i%2==0){
 //         console.log(i);
 //     }
 // }
-let i = 0;
-let donkey = ["amit", "shah", "niles"];
-for(i;i<=donkey.length;i++){
-    console.log(donkey[i]);
-}
-//function without argument and without return type
+// let i = 0;
+// let donkey = ["amit", "shah", "niles"];
+// for(i;i<=donkey.length;i++){
+//     console.log(donkey[i]);
+// }
+// // function without argument and without return type
 // function add(){
 //     let a = 23;
 //     let b = 34;
 //     let return = a + b;
 //     console.log(return);
 // }
-//function with argument and without return type
-function add (x,y){
-    let result = x + y;
-    console.log(result);
-}
-add(10,20);
-//function without argument and with return type
-function add(){
-    
-}
+// // function with argument and without return type
+// function add (x,y){
+//     let result = x + y;
+//     console.log(result);
+// }
+// add(10,20);
+// // function without argument and with return type
+// function add(){
+//     let a = 1; 
+//     let b = 2; 
+//     let c = 3;
+//      let result = a + b + c;
+//      return result
+// }
+// console.log(add());
+//find odd even by using function
+//date display
+let date = new Date();
+console.log(date.getTime());
